f227df9f1f75ee6146679f435197e4b62ed26ad9
Ganesh Dandekar <ganesh15122002@gmail.com>
2025-02-22 13:07:40 +0530

    user login, register

--- server/src/mainRouter.js
+++ server/src/mainRouter.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import userRouter from './routers/userRouter.js';
+import eventRouter from './routers/eventRouter.js';
+import adminRouter from './routers/adminRouter.js';
 const mainRouter = express.Router();
 
+
+mainRouter.use('/admin', adminRouter);
+mainRouter.use('/user', userRouter);
+mainRouter.use('/event', eventRouter);
 
 
 
