31023641f2c1435fa325b4b1b3b002bbdc0823c2
Tejas Surse <118660087+TejasSurse@users.noreply.github.com>
2025-02-22 14:06:49 +0530

    admin routes - register, login, logout

--- server/src/models/adminModel.js
+++ server/src/models/adminModel.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
 
 const adminSchema = new mongoose.Schema({
   adminFullName: {
@@ -9,28 +11,23 @@ const adminSchema = new mongoose.Schema({
   adminEmail: {
     type: String,
     required: true,
+    unique: true,
   },
   adminPassword: {
     type: String,
     required: true,
   },
-
   adminMobileNumber: {
     type: String,
   },
-
   adminBio: {
     type: String,
     maxlength: 1000,
-    
   },
-
   adminSection: {
     type: String,
     maxlength: 100,
-
   },
-
   adminPhoto: {
     public_url: {
         type: String,
@@ -42,45 +39,39 @@ const adminSchema = new mongoose.Schema({
         type: String,
     },
   },
-
   adminRole: {
      type: String,
   },
-
-
   adminForgotPasswordToken: {
     type: String
   },
-
   adminPasswordToken: {
     type: String
   }
-
-
 }, { timestamps: true });
 
+adminSchema.methods.isPasswordMatch = async function(password) {
+    return await bcrypt.compare(password, this.adminPassword);
+};
 
-adminSchema.methods = {
-    async isPasswordMatch(password) {
-        return await bcrypt.compare(password, this.adminPassword);
-    }
-}
+adminSchema.methods.generateJWTToken = function() {
+    return jwt.sign(
+        { id: this._id, email: this.adminEmail },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "24h",
+        }
+    );
+};
 
 adminSchema.pre("save", async function (next) {
-
     if(!this.isModified("adminPassword")) {
         return next();
     }
-
     this.adminPassword = await bcrypt.hash(this.adminPassword, 10);
     next();
+});
 
-  }
-)
+const Admin = mongoose.model('Admin', adminSchema);
 
-
-
-
-
-export const Admin = mongoose.model('Admin', adminSchema);
-
+export default Admin;
--- server/src/routers/adminRouter.js
+++ server/src/routers/adminRouter.js
@@ -1,6 +1,18 @@
 import express from 'express';
 const adminRouter = express.Router();
 
-      
+import { upload } from '../middlewares/multerMiddleware.js';
+import { register, login, logout } from '../controllers/v1/adminController.js';
+
+
+adminRouter.route("/register")
+            .post(upload.none(), register);
+
+adminRouter.route("/login")
+            .post(upload.none(), login);
+
+adminRouter.route("/logout")  
+            .get(logout);
+
 
 export default adminRouter;
