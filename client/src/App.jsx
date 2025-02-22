0abbf09dcaf3636d71a0cc319972d76ff5c2d27f
Pravin Chavan <159113280+Pravinchavan24@users.noreply.github.com>
2025-02-22 11:44:55 +0530

    setup project

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+
+      <h2 className=" bg-gray-200 ">
+        this is the new code 
+      </h2>
+    
+    </>
+  )
+}
+
+export default App
